196d8e60de7e359c8c772964a53653d61be5e817
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-12-17 10:05:43 +0100

    anatomy of unit tests

---
+++ src/UnitTestingExample/MyTests.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestingExample
+{
+    public class StringTests
+    {
+        [AwesomeTest]
+        public void Trim_method_should_remove_chars_in_the_beginning_and_end_of_string()
+        {
+            //arange
+            var input = "abcd ";
+            //act
+            var result = input.Trim(new[] { ' ', 'a' });
+
+            // assert
+            MyAsserts.IsEqual("bcd", result);
+        }
+    }
+
+    public class TaxProviderTests
+    {
+
+    }
+}
---
+++ src/UnitTestingExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var tests = Assembly.GetCallingAssembly()
+               .GetTypes()
+               .Where(t => !t.IsAbstract)
+               .Where(t => t.IsClass)
+               .Where(t => t.IsPublic)
+               .SelectMany(t => t.GetMethods())
+               .Where(m => m.IsPublic)
+               .Where(m => m.GetCustomAttribute<AwesomeTestAttribute>() != null);
+
+            foreach (var test in tests)
+            {
+                Console.WriteLine($"found test: {test.Name}");
+            }
+
+
+
+        }
+
+
+
+      
+    }
+
+    public class AwesomeTestAttribute : Attribute
+    {
+    }
+
+    public static class MyAsserts
+    {
+        public static void IsEqual(string expected, string result)
+        {
+            var c = Console.ForegroundColor;
+
+            if (expected.Equals(result))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pass");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Fail");
+            }
+
+            Console.ForegroundColor = c;
+        }
+    }
+}
